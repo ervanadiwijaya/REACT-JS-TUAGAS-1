745311c78f0a58b8fdb1a6cc5522fb5a41f23dc3
jauhhari <ervanadiwijaya@gmail.com>
2021-07-02 14:12:09 +0700

    ok

---
+++ src/Page/MenuTentangKami.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MenuAbout extends Component {
+    render() {
+        return (
+            <p>Ini Halaman Menu Tentang Kami</p>
+        );
+    }
+}
+
+export default MenuAbout;
